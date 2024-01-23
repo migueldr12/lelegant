25ff3326b52b2c268d55c3a05f0da2744ae4b516
migueldr12 <110634700+migueldr12@users.noreply.github.com>
2024-01-22 23:22:07 -0600

    Login validacion
    Se agrega validación de campos al formulario ademas de una alerta si el usuario no existe.

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,30 +1,56 @@
-const login = function(event){
-    event.preventDefault();
+const login = function (event) {
+  event.preventDefault();
 
-    const user = document.getElementById('txtUser').value;
-    const pass = document.getElementById('txtPassword').value;
-        
+  const userValue = document.getElementById('txtUser').value;
+  const passValue = document.getElementById('txtPassword').value;
+  const userInput = document.getElementById('txtUser');
+  const passInput = document.getElementById('txtPassword');
+  const blockAlert = document.getElementById('txtAlerta');
+
+  userInput.classList.remove('is-invalid');
+  passInput.classList.remove('is-invalid');
+  blockAlert.style.display = 'none';
+
+  if (userValue === '') {
+    userInput.classList.add('is-invalid');
+    blockAlert.style.display = 'block';
+  } else if (passValue === '') {
+    passInput.classList.add('is-invalid');
+    blockAlert.style.display = 'block';
+  } else {
     const appContext = window.location.pathname.split('/')[1];
 
-    fetch(`/${appContext}/api/login/log?txtUser=${user}&txtPassword=${pass}`)
-        .then(response => {
-            if (response.ok) {
-                return response.json();  
-            } else {
-                throw new Error(`Error en la solicitud: ${response.statusText}`);
+    fetch(
+      `/${appContext}/api/login/log?txtUser=${userValue}&txtPassword=${passValue}`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+      })
+      .then(data => {
+        if (data > 0) {
+          location.href = './admin/index.html';
+        } else {
+          Swal.fire({
+            text: 'Usuario no encontrado',
+            icon: 'info',
+            confirmButtonText: 'Intentar de nuevo',
+          }).then(result => {
+            if (result.isConfirmed) {
+              userInput.value = '';
+              passInput.value = '';
             }
-        })
-        .then(data => {
-            if(data > 0){
-                location.href = "./admin/index.html";
-            } else{
-                alert('Usuario no encontrado');
-            }
-        })
-        .catch(error => {
-            // Manejar errores de red u otros errores
-            console.error('Error en la solicitud:', error);
-        });
+          });
+        }
+      })
+      .catch(error => {
+        // Manejar errores de red u otros errores
+        console.error('Error en la solicitud:', error);
+      });
+  }
 };
 
 
